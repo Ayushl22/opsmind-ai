122d7d918830e508311ade17075e42e5d94ce8eb
nupoor-mahajan <nupoormahajan06@gmail.com>
2026-05-06 18:10:53 +0530

    week4: register conversation API routes

--- backend/src/index.js
+++ backend/src/index.js
@@ -2,17 +2,21 @@ import dotenv from "dotenv";
 dotenv.config();
 
 import express from "express";
+import cors from "cors";
 import connectDB from "./config/db.js";
 import uploadRoutes from "./routes/upload.js";
 import queryRoutes from "./routes/query.js";
+import conversationRoutes from "./routes/conversations.js";
 
 connectDB();
 
 const app = express();
+app.use(cors());
 app.use(express.json());
 
 app.use("/api", uploadRoutes);
 app.use("/api", queryRoutes);
+app.use("/api", conversationRoutes);
 
 app.get("/", (req, res) => {
   res.send("OpsMind AI Backend Running ");
